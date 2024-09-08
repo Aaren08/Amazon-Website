1ca9b002a079860e23b22531728fd67a46a7c988
Abdal <schneiderneil392@gmail.com>
2024-09-09 03:05:01 +0500

    Use products.js file

--- javascript-amazon-project/javascript-amazon-project-main/Scripts/amazon.js
+++ javascript-amazon-project/javascript-amazon-project-main/Scripts/amazon.js
@@ -1,30 +1,6 @@
 // DATA STUCTURE
 
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}]
+// Loaded products from products.js by putting it above amazon.js
 
 // 2. Combining all the HTML together into one string
 
@@ -83,7 +59,6 @@ products.forEach( (product) => {
             Add to Cart
           </button>
         </div>`
-
     })
     
 // 3. putting the products on a webpage --> DOM
