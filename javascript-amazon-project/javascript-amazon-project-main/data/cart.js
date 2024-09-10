07470a9fe64a4c870a8ff8436ec3e9fa62c3bc06
Abdal <schneiderneil392@gmail.com>
2024-09-11 03:18:06 +0500

    Save cart in localStorage

--- javascript-amazon-project/javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project/javascript-amazon-project-main/data/cart.js
@@ -1,12 +1,23 @@
 // 5. Separate file for cart
 
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}]
+export let cart = JSON.parse(localStorage.getItem('cart'))
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }]
+}
+
+
+// 9.  Setting localStorage to store items in cart
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
+
 
 export function addToCart(productId) {
     let matchingItem
@@ -26,6 +37,7 @@ export function addToCart(productId) {
         quantity: 1
       })
     }
+    saveToStorage()
 }
 
 // Removing Items from checkout page
@@ -38,4 +50,6 @@ export function removeFromCart(productId) {
     }
   })
   cart = newCart
+  saveToStorage()
 }
+
