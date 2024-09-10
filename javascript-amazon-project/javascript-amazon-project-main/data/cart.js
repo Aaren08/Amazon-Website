30a913f77bb4405701984c5baf5acadad2834166
Abdal <schneiderneil392@gmail.com>
2024-09-11 03:00:41 +0500

    Delete products from cart when clicking delete

--- javascript-amazon-project/javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project/javascript-amazon-project-main/data/cart.js
@@ -1,6 +1,6 @@
 // 5. Separate file for cart
 
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
@@ -26,4 +26,16 @@ export function addToCart(productId) {
         quantity: 1
       })
     }
-  }
+}
+
+// Removing Items from checkout page
+
+export function removeFromCart(productId) {
+  const newCart = []
+  cart.forEach( (cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem)
+    }
+  })
+  cart = newCart
+}
